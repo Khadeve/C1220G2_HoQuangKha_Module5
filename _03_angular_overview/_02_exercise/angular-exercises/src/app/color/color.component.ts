6cbbb0cb5468bd63fcad1d1c5d16db40e8e6761f
Khadeve <hoquangkha95@gmail.com>
2021-06-07 08:03:30 +0700

    up task 7/6

--- _03_angular_overview/_02_exercise/angular-exercises/src/app/color/color.component.ts
+++ _03_angular_overview/_02_exercise/angular-exercises/src/app/color/color.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ColorComponent implements OnInit {
   colorValue: string = '#FF0000';
+  redValue: number = 255;
+  greenValue: number = 0;
+  blueValue: number = 0;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  getHexFromDec(n: number): string {
+    let hex: string = n.toString(16); // convert from decimal to hexadecimal.
+    return (hex.length === 1) ? (0 + hex) : hex;  // prepend 0 if there is 1 digit.
+  }
+
+  changeColor(): void {
+    this.colorValue = '#'  + this.getHexFromDec(this.redValue)
+                           + this.getHexFromDec(this.greenValue)
+                           + this.getHexFromDec(this.blueValue);
+  }
 }
--- case_study/furama-resort/src/app/customer-list/customer-list.component.ts
+++ case_study/furama-resort/src/app/customer-list/customer-list.component.ts
@@ -11,6 +11,8 @@ export class CustomerListComponent implements OnInit {
   customerList: Customer[] = [
     {id: 'KH-01', name: "Nguyen Huy", dateOfBirth: "23/01/1992", gender: "male", idCard: "123456789", phoneNumber: "0911111111", email: "nguyenhuy@gmail.com", address: "Da Nang", customerType: "gold"},
     {id: 'KH-02', name: "Thanh Cong", dateOfBirth: "23/02/1992", gender: "female", idCard: "123456787", phoneNumber: "0911111112", email: "thanhcong@gmail.com", address: "Da Nang", customerType: "member"},
+    {id: 'KH-03', name: "Quoc Tuan", dateOfBirth: "23/02/1992", gender: "female", idCard: "123456787", phoneNumber: "0911111112", email: "quoctuan@gmail.com", address: "Quang Tri", customerType: "silver"},
+    {id: 'KH-04', name: "Quoc Tu", dateOfBirth: "23/02/1992", gender: "female", idCard: "123456787", phoneNumber: "0911111112", email: "quoctuan@gmail.com", address: "Quang Tri", customerType: "silver"}
   ];
 
   page: number = 1;
@@ -19,5 +21,26 @@ export class CustomerListComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  searchByName(event: Event): void {
+    let keyword: string = (event.target as HTMLInputElement).value;
+    let customerArr: Array<Customer> = [];
+
+    this.customerList = [
+      {id: 'KH-01', name: "Nguyen Huy", dateOfBirth: "23/01/1992", gender: "male", idCard: "123456789", phoneNumber: "0911111111", email: "nguyenhuy@gmail.com", address: "Da Nang", customerType: "gold"},
+      {id: 'KH-02', name: "Thanh Cong", dateOfBirth: "23/02/1992", gender: "female", idCard: "123456787", phoneNumber: "0911111112", email: "thanhcong@gmail.com", address: "Da Nang", customerType: "member"},
+      {id: 'KH-03', name: "Quoc Tuan", dateOfBirth: "23/02/1992", gender: "female", idCard: "123456787", phoneNumber: "0911111112", email: "quoctuan@gmail.com", address: "Quang Tri", customerType: "silver"},
+      {id: 'KH-04', name: "Quoc Tu", dateOfBirth: "23/02/1992", gender: "female", idCard: "123456787", phoneNumber: "0911111112", email: "quoctuan@gmail.com", address: "Quang Tri", customerType: "silver"}
+    ];
+    this.totalCustomers = this.customerList.length;
+
+    for (const customer of this.customerList) {
+      if (customer.name.toLowerCase().search(keyword.toLowerCase()) !== -1) {
+        customerArr.push(customer);
+      }
+    }
+    this.customerList = customerArr;
+    this.totalCustomers = this.customerList.length;
   }
 }
